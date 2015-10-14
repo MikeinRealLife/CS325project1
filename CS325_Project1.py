1ffd72ea14951053e486d0f20de786634864e9b6
MikeinRealLife <michaelloo@Michaels-MacBook-Pro.local>
2015-10-13 19:26:42 -0700

    made changes, realized that enumerate was actually better enumerate

--- CS325_Project1.py
+++ CS325_Project1.py
@@ -13,8 +13,32 @@ def file_reader():
     
     #return test data
     return test_data
-    
+
 def enumerate_case(array):
+    
+    #initialize max sum
+    max_sum = 0
+
+    #first iteration
+    for x in range(0, len(array)):
+        #initialize total
+        
+        
+        #second interator
+        for i in range(x, len(array)):
+            total = 0
+            #sum total
+            for y in range(x, i):
+                total += array[y]
+            
+            #if sum is larger than max_sum, set new max_sum
+            if total > max_sum:
+                max_sum = total
+
+    return max_sum
+
+#I think this is actually the better enum case?
+def better_enumerate_case(array):
     
     #initialize max sum
     max_sum = 0
@@ -34,13 +58,7 @@ def enumerate_case(array):
             if total > max_sum:
                 max_sum = total
                 
-                '''
-                #this line tells you the values in your max array, but it
-                #will probably mess with the time value so its commented out.
-                sum_arr = [j for j in array[x:i+1]]
-                '''
-
-    return max_sum#, sum_arr
+    return max_sum
     
 def d_and_c(arr, max_sum):
     
@@ -59,19 +77,18 @@ def d_and_c(arr, max_sum):
             
         return max(d_and_c(arr[:mid], max_sum), d_and_c(arr[mid:], max_sum))
         
-        
-    
+          
 def enumerate_test(test_packet):
     max_vals = []
     #max_arrs = []
     
     for line in test_packet:
         #mx_val, mx_arr = enumerate_case(line)
-        mx_val = enumerate_case(line)
+        mx_val = better_enumerate_case(line)
         max_vals.append(mx_val)
         #max_arrs.append(mx_arr)
         
-    return max_vals#, max_arrs
+    return max_vals
         
 test_data = file_reader()
     
