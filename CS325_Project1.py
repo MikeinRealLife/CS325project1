6db9fa3eb3a27ff5e9286863dec5cf68b417d71c
MikeinRealLife <michaelloo@Michaels-MacBook-Pro.local>
2015-10-13 23:03:40 -0700

    now everything prints out the arrays too

--- CS325_Project1.py
+++ CS325_Project1.py
@@ -18,7 +18,7 @@ def enumerate_case(array):
     
     #initialize max sum
     max_sum = 0
-
+    max_array = []
     #first iteration
     for x in range(0, len(array)):
         #initialize total
@@ -27,18 +27,18 @@ def enumerate_case(array):
         #second interator
         for i in range(x, len(array)):
             total = 0
+            array_test = []
             #sum total
             for y in range(x, i):
                 total += array[y]
-            
+                array_test.append(array[y])
+                
             #if sum is larger than max_sum, set new max_sum
             if total > max_sum:
                 max_sum = total
-                
-                #this line tells you the values in your max array
-                sum_arr = [j for j in array[x:i+1]]
+                max_array = array_test
 
-    return max_sum, sum_arr
+    return max_sum, max_array
 
 #I think this is actually the better enum case?
 def better_enumerate_case(array):
@@ -67,10 +67,11 @@ def better_enumerate_case(array):
                 
     return max_sum, sum_arr
     
-def d_and_c(arr, max_sum):
+def d_and_c(arr, max_sum, max_arr):
     
     if len(arr) == 1:
-        return max_sum
+        return max_sum, max_arr
+        
     else:
         mid = int(len(arr)/2)
         arr1 = arr[:mid]
@@ -78,61 +79,79 @@ def d_and_c(arr, max_sum):
         
         sum1 = 0
         max_sum1 = 0
+        val_arr1 = []
+        max_arr1 = []
         for x in range(len(arr1)-1, -1, -1):
             sum1 += arr1[x]
-            max_sum1 = max(max_sum1, sum1)
+            val_arr1.append(arr1[x])
+            if max_sum1 < sum1:
+                max_sum1 = sum1
+                max_arr1 = val_arr1[::-1]
             
         sum2 = 0
         max_sum2 = 0
+        val_arr2 = []
+        max_arr2 = []
         for x in range(0, len(arr2)):
             sum2 += arr2[x]
-            max_sum2 = max(max_sum2, sum2)
+            val_arr2.append(arr2[x])
+            if max_sum2 < sum2:
+                max_sum2 = sum2
+                max_arr2 = val_arr2[:]
             
         sum3 = max_sum1 + max_sum2
+        max_arr3 = max_arr1 + max_arr2
         
-        max_sum = max(max_sum, max_sum1, max_sum2, sum3)
+        if max_sum1 > max_sum:
+            max_sum = max_sum1
+            max_arr = max_arr1
             
-        return max(d_and_c(arr[:mid], max_sum), d_and_c(arr[mid:], max_sum))
+        if max_sum2 > max_sum:
+            max_sum = max_sum2
+            max_arr = max_arr2
+            
+        if sum3 > max_sum:
+            max_sum = sum3
+            max_arr = max_arr3
+                
+        return max(d_and_c(arr[:mid], max_sum, max_arr), d_and_c(arr[mid:], max_sum, max_arr))
         
           
 def enumerate_test(test_packet):
     max_vals = []
-    #max_arrs = []
+    max_arrs = []
     
     for line in test_packet:
-        #mx_val, mx_arr = enumerate_case(line)
-        mx_val = better_enumerate_case(line)
+        mx_val, mx_arr = enumerate_case(line)
         max_vals.append(mx_val)
-        #max_arrs.append(mx_arr)
+        max_arrs.append(mx_arr)
         
-    return max_vals
+    return max_vals, max_arrs
     
 def linear_time(arr):
     
     max_sum = 0
     total = 0
+    sum_array = []
     i = 0
+    
     for j in range(0, len(arr)):
         total += arr[j]
-        print(total)
-        
+        if arr[j] > total:
+            total = arr[j]
+            i = j
+            
         if total > max_sum:
             max_sum = total
+            sum_array = [arr[x] for x in range(i, j+1)]
+
             
-        while total <= max_sum and i < j:
-            total -= arr[i]
-            i+=1
-        
-     
-    return max_sum
+    return max_sum, sum_array
         
         
         
 test_data = file_reader()
-    
-val = d_and_c(test_data[0], 0)
+testar = []
+val, val2 = d_and_c(test_data[0], 0, testar)
 
-max_vals = enumerate_test(test_data)
-
-test = linear_time(test_data[0])
-print('Answer is: ' +str(test))
+max_vals, max_arrs = enumerate_test(test_data)
